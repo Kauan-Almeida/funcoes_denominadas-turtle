ff3d47fd2acbf11bae89d0373dbba34eaa1d346e
Kauan-Almeida <kauanalmeida2233@gmail.com>
2022-12-08 19:10:21 -0300

    funcoes_denominadas

---
+++ Turtle.py
@@ -0,0 +1,13 @@
+from turtle import *
+#movimentando a tartaruga
+shape("turtle") #coloca o formato da tartaruga
+delay(50)
+forward(100) #anda para frente 100 pixels
+right(90) #vira 90 graus para direita
+forward(200)
+right(90)
+forward(100)
+right(90)
+forward(200)
+
+
